0c9637124e979c65a27dd07305c0799985411f3d
Aadit Singal <79838616+adi-devv@users.noreply.github.com>
2024-04-27 16:17:27 +0530

    Delete assets directory

--- assets/js/scroll.js
+++
@@ -1,8 +0,0 @@
-
-$(window).on('scroll', function () {
-    if ($(window).scrollTop() < 100) {
-        $('nav').removeClass('sticky-nav');
-    } else {
-        $('nav').addClass('sticky-nav');
-    }
-});
